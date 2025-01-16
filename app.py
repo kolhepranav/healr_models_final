b5ca4622fa3f3983e84a99d9812d978bc8973aae
Pranav Kolhe <pranavkolhe77@gmail.com>
2025-01-16 19:02:28 +0530

    update app.py

--- app.py
+++ app.py
@@ -602,16 +602,16 @@ async def get_prescriptions(
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-
-
-
 # if __name__ == "__main__":
 #     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+#     uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 
 
